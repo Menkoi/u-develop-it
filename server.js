8f837324be8e965678e3c5b7e7c89899cec00589
celaeon <celaeon@gmail.com>
2021-02-21 20:15:51 -0500

    working all/single/delete candidates

--- server.js
+++ server.js
@@ -1,23 +1,94 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
+const sqlite3 = require('sqlite3').verbose();
 
 //Express Middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
-app.get ('/', (req, res) => {
-    res.json({
-        message: 'hello world'
-    });
+
+//connect to new database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected to the election database');
 });
+
+//GET all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+//GET single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates 
+               WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+//GET delete candidates
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+  });
+
+// creater a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+ //           VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+
+//ES5 function, not arrow function, to use this
+//db.run(sql, params, function(err,result) {
+ //   if (err) {
+ //       console.log(err);
+ //   }
+ //   console.log(result, this.lastID);
+//});
 
 //Default response for any other request (not found) catch all
 app.use((req, res) => {
     res.status(404).end();
 });
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+//start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+})
